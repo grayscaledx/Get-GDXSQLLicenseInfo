720dd181bee410a1cf005ca02f57b075eaad37b6
Nick <grayscale.dx@gmail.com>
2015-02-08 21:45:19 -0500

    Uploaded initial script base
    NOTE: THIS PROBABLY DOES NOT WORK.
    
    This is to move a repo to my desktop.

---
+++ Get-GDXSQLLicenseInfo.ps1
@@ -0,0 +1,45 @@
+﻿Function Get-GDXSQLLicenseInfo{
+
+    [CmdletBinding()]
+    param(
+    [string[]]$ComputerName = $env:COMPUTERNAME
+    )
+
+    BEGIN {
+    
+        $collection = @()
+
+    }
+
+    PROCESS {
+    
+        foreach($c in $ComputerName){
+            
+            try {
+
+                $SQLNameSpaces = Get-WmiObject -Namespace ROOT\Microsoft\SqlServer -Class __NAMESPACE -ComputerName $c
+                Write-Output $SQLNameSpaces
+
+                foreach ($cmclass in $(($SQLNameSpaces.Name) -match "ComputerManagement")){
+                
+                    $cmquery = Get-WmiObject -Namespace ($cmclass.__NAMESPACE)\($cmclass.Name) -Class SqlServiceAdvancedProperty   
+                    $collection += $cmquery  
+                
+                }
+
+                
+            } catch {
+
+            }
+        
+        }
+    
+    }
+
+    END {
+    
+        Write-Output $collection
+    
+    }
+
+}
